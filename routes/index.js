02c87b68c29dbdf0cd90785ec2efbfc800150b8e
Jfilippo06 <jfilippo04@gmail.com>
2022-10-19 20:04:10 +0200

    index router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const homeRouter = require('./home');
+const usersRouter = require('./users');
+
+router.use('/', homeRouter);
+router.use('/users', usersRouter);
 
 module.exports = router;
