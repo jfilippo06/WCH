1d379ee7652545305da540682d33b4c9491216e1
Jfilippo06 <jfilippo04@gmail.com>
2022-10-21 00:31:34 +0200

    appError

--- DAL/auth.js
+++ DAL/auth.js
@@ -1,3 +1,4 @@
+const AppError = require('../errors/not-found')
 const {User} = require('../models')
 
 const registerUser = async (email, userName, hash, roleId) => {
@@ -6,7 +7,7 @@ const registerUser = async (email, userName, hash, roleId) => {
             email: email
         }
     })
-    if (user) throw new Error('Usuario ya existe')
+    if (user) throw new AppError('Authentication failed! Email / password does not correct.', 201);
     const data = await User.create({
         email,
         userName,
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,13 @@
-const { registerService } = require("../services/auth")
+const { registerService } = require('../services/auth')
 
-const registerController = async (req,res) => {
-    const {email, userName, password, roleId} = req.body
-    const data = await registerService(email, userName, password, roleId)
-    return res.status(201).json(data)
+const registerController = async (req, res, next) => {
+    try {
+        const {email, userName, password, roleId} = req.body
+        const data = await registerService(email, userName, password, roleId)
+        return res.status(200).json(data)
+    } catch (err) {
+        res.status(err.code).json(err.message)
+    }
 }
 
 module.exports = {
