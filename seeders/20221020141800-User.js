92d71bd1e0d9bac7c84d6c73762db2563d2d827a
Jfilippo06 <jfilippo04@gmail.com>
2022-10-20 17:10:40 +0200

    user bcrypt

--- seeders/20221020141800-User.js
+++ seeders/20221020141800-User.js
@@ -1,13 +1,26 @@
 'use strict';
+// require('dotenv').config()
+const bcrypt = require('bcrypt')
+const auth = require('../config/auth')
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
+    const salt = await bcrypt.genSalt(Number.parseInt(auth.rounds))
+    const admin = await bcrypt.hash(auth.admin, salt)
+    const standard = await bcrypt.hash(auth.standard, salt)
      await queryInterface.bulkInsert('Users', [
       {
        userName: 'Admin',
-       password: 'Admin',
+       password: admin,
        roleId: 1,
+       createdAt: new Date,
+       updatedAt: new Date,
+      },
+      {
+       userName: 'Standard',
+       password: standard,
+       roleId: 2,
        createdAt: new Date,
        updatedAt: new Date,
       },
