70476b4745597d7f99a397476d6b7b08c0af7e1f
Jfilippo06 <jfilippo04@gmail.com>
2022-10-20 18:14:01 +0200

    register

--- middlewares/validationBody.js
+++ middlewares/validationBody.js
@@ -1,8 +1,17 @@
-const bodyAuth = [
+const { body } = require("express-validator");
+
+const bodyLogin = [
     body("userName", "Enter a valid userName!").trim().notEmpty().escape(),
     body("password", "Enter a valid password!").trim().isLength({ min: 5 }).escape(),
 ];
 
+const bodyRegister = [
+    body("userName", "Enter a valid userName!").trim().notEmpty().escape(),
+    body("password", "Enter a valid password!").trim().isLength({ min: 5 }).escape(),
+    body("roleId", "Enter a valid userName!").trim().notEmpty().escape(),
+];
+
 module.exports = {
-    bodyAuth,
+    bodyLogin,
+    bodyRegister,
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const validationResult = require('../middlewares/validationResult')
+const { bodyRegister } = require('../middlewares/validationBody');
 const router = express.Router()
 
 router.post('/login');
-router.post('/register');
+router.post('/register', bodyRegister, validationResult);
 
 module.exports = router
