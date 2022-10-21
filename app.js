ff31f6d65b564de8e8254462b3f23c7d773830af
Jfilippo06 <jfilippo04@gmail.com>
2022-10-21 17:48:51 +0200

    csurf

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const csrf = require('csurf')
 
 const indexRouter = require('./routes');
 
@@ -17,6 +18,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(csrf({cookie: true}))
+app.use((req,res,next) => {
+    res.locals.csrfToken = req.csrfToken()
+    next()
+})
 
 app.use('/', indexRouter);
 
