eddf8e046fb2abd9a1a9e24987d57a0f6b31d5e5
Jfilippo06 <jfilippo04@gmail.com>
2022-11-01 20:33:35 +0100

    RoleId

--- migrations/20221020011803-create-user.js
+++ migrations/20221020011803-create-user.js
@@ -18,7 +18,7 @@ module.exports = {
       password: {
         type: Sequelize.STRING
       },
-      roleId: {
+      RoleId: {
         type: Sequelize.INTEGER,
         references: {
           model: 'Roles',
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const { bodyRegister, bodyLogin } = require('../middlewares/validationBody');
 const validationResultAuth = require('../middlewares/validationResult-auth')
+const validationResult = require('../middlewares/validationResult')
 const asyncHandler = require('../middlewares/async-handler');
 const loginUser = require('../middlewares/loginUser');
 const { registerController, loginController, logoutController } = require('../controllers/auth');
 const router = express.Router()
 
 router.post('/login', bodyLogin, validationResultAuth, asyncHandler(loginController));
-router.post('/register', bodyRegister, validationResultAuth, asyncHandler(registerController));
+router.post('/register', bodyRegister, validationResult, asyncHandler(registerController));
 router.get('/logout', loginUser, asyncHandler(logoutController))
 
 module.exports = router
