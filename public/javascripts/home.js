0ec1add7e83bd7ce902de36c732a6f2806746e38
Jfilippo06 <jfilippo04@gmail.com>
2022-11-05 17:31:19 +0100

    modal

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -3,13 +3,17 @@ const main = document.querySelector('#main')
 
 salir.addEventListener('click', () => {
     main.innerHTML = `
-    <div class="card-body">
-        <h5 class="card-title">Default Card</h5>
-        <div class="alert alert-success alert-dismissible fade show" role="alert"> 
-            <a href="/home"> Permanecer en la sesion </a> 
-        </div>
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            <a href="/auth/logout"> Salir de la sesión </a> 
+    <div class="modal modal-sheet position-static d-block bg-secondary py-5" tabindex="-1" role="dialog" id="modalSheet">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content rounded-4 shadow">
+                <div class="modal-header border-bottom-0">
+                    <h1 class="modal-title fs-5">¿Salir de la sesión?</h1>
+                </div>
+                <div class="modal-footer flex-column border-top-0">
+                    <a class="btn btn-lg btn-primary w-100 mx-0" href="/auth/logout">Si</a>
+                    <a class="btn btn-lg btn-danger w-100 mx-0" href="/home">No</a>
+                </div>
+            </div>
         </div>
     </div>
     `
