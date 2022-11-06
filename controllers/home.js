db0d0b72ff3be870d5f1ec1b20a8f5494ee18f3d
Jfilippo06 <jfilippo04@gmail.com>
2022-11-06 17:05:06 +0100

    exit

--- controllers/home.js
+++ controllers/home.js
@@ -2,6 +2,11 @@ const index = async (req,res) => {
     res.render('pages/index');
 }
 
+const exit = async (req,res) => {
+    res.render('pages/exit');
+}
+
 module.exports = {
     index,
+    exit,
 }
--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { index } = require('../controllers/home');
+const { index, exit } = require('../controllers/home');
 const asyncHandler = require('../middlewares/async-handler');
 const router = express.Router();
 
 router.get('/', asyncHandler(index));
+router.get('/salir', asyncHandler(exit));
 
 module.exports = router;
