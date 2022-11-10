c177a1757954fad9859e2add08eeddd79e97e0ee
Jfilippo06 <jfilippo04@gmail.com>
2022-11-10 19:19:17 +0100

    index

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ const homeRouter = require('./home');
 const authRouter = require('./auth');
 const inventarioRouter = require('./inventario');
 const usuarioRouter = require('./usuario');
+const mantenimientoRouter = require('./mantenimiento');
 
 router.use('/', homeRouter);
 router.use('/auth', authRouter);
 router.use('/inventario', inventarioRouter);
 router.use('/usuario', usuarioRouter);
+router.use('/mantenimiento', mantenimientoRouter);
 
 module.exports = router;
