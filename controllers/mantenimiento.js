5065e1435f4ca10d513183ac47ff7e238049de55
Jfilippo06 <jfilippo04@gmail.com>
2022-11-10 23:19:31 +0100

    copy

--- controllers/mantenimiento.js
+++ controllers/mantenimiento.js
@@ -1,7 +1,23 @@
+const { copyFile, constants } = require('node:fs/promises')
+const path = require('path');
+const file = path.join(__dirname, '../database.sqlite')
+const dest = path.join(__dirname, '../backup/database2.sqlite')
+
 const respaldarGet = async (req, res) => {
     res.render('pages/respaldar');
 }
 
+const copy = async (req, res) => {
+    try {
+        await copyFile(file, dest)
+        req.flash("success", { msg: "Base de datos" });
+        res.redirect('/mantenimiento/respaldar')
+    } catch (error) {
+        res.json(error.message)
+    }
+}
+
 module.exports = {
     respaldarGet,
+    copy,
 }
--- routes/mantenimiento.js
+++ routes/mantenimiento.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { respaldarGet } = require('../controllers/mantenimiento');
+const { respaldarGet, copy } = require('../controllers/mantenimiento');
 const asyncHandler = require('../middlewares/async-handler');
 const loginUser = require('../middlewares/loginUser');
 const router = express.Router();
 
 router.get('/respaldar', loginUser, asyncHandler(respaldarGet))
+router.get('/copy', loginUser, asyncHandler(copy))
 
 module.exports = router;
