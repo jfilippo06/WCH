10ae8a95a04ee4762802389f1e8118cd74e8f1ae
Jfilippo06 <jfilippo04@gmail.com>
2022-11-14 19:45:58 +0100

    model factura_cliente

---
+++ migrations/20221114183903-create-factura-producto.js
@@ -0,0 +1,35 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Factura_productos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      Id_cliente: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Clientes',
+          key: 'id'
+        },
+      },
+      orden: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Factura_productos');
+  }
+};
--- models/factura_producto.js
+++ models/factura_producto.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Cliente extends Model {
+  class Factura_producto extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -11,15 +11,14 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Cliente.hasOne(models.Factura_producto)
+      Factura_producto.belongsTo(models.Cliente)
     }
   }
-  Cliente.init({
-    nombre: DataTypes.STRING,
-    cedula: DataTypes.INTEGER
+  Factura_producto.init({
+    orden: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'Cliente',
+    modelName: 'Factura_producto',
   });
-  return Cliente;
+  return Factura_producto;
 };
