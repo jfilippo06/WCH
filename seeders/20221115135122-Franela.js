b33618a1e05200079fac1e663b74eb766103b07a
Jfilippo06 <jfilippo04@gmail.com>
2022-11-15 15:11:11 +0100

    seeder franela

---
+++ seeders/20221115135122-Franela.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "Franelas",
+      [
+        {
+          
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Franelas", null, {});
+  },
+};
