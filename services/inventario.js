8f2f8299eda651a460a15e413ad7806efeeb1184
Jfilippo06 <jfilippo04@gmail.com>
2022-11-18 02:56:53 +0100

    helper

--- services/inventario.js
+++ services/inventario.js
@@ -8,8 +8,22 @@ const franelaService = async (page, size, opcion, valor) => {
   const { count: totalItems, rows: franelas } = data;
   const currentPage = page ? +page : 0;
   const totalPages = Math.ceil(totalItems / limit);
-  const next = nextPage("inventario/franela", currentPage, totalPages, limit);
-  const prev = prevPage("inventario/franela", currentPage, totalPages, limit);
+  const next = nextPage(
+    "inventario/franela",
+    currentPage,
+    totalPages,
+    limit,
+    opcion,
+    valor
+  );
+  const prev = prevPage(
+    "inventario/franela",
+    currentPage,
+    totalPages,
+    limit,
+    opcion,
+    valor
+  );
   return { totalItems, franelas, prev, next };
 };
 
