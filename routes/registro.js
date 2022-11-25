887746028ba1f3db2f5e42e28e80ff2b60f3b8f6
Jfilippo06 <jfilippo04@gmail.com>
2022-11-25 21:02:20 +0100

    registro router

---
+++ routes/registro.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const asyncHandler = require('../middlewares/async-handler');
+const loginUser = require('../middlewares/loginUser');
+const router = express.Router();
+
+module.exports = router;
