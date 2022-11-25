992cd36d716eeda1c0769b48b1ebf84baa167dba
Jfilippo06 <jfilippo04@gmail.com>
2022-11-25 21:08:37 +0100

    registro services

--- controllers/registro.js
+++ controllers/registro.js
@@ -1,4 +1,10 @@
-const getFranela = async (req, res) => {}
+const getFranela = async (req, res) => {
+    try {
+        const { page, size, opcion, valor } = req.query;
+    } catch (error) {
+        res.json(error.message)
+    }
+}
 
 module.exports = {
     getFranela,
--- routes/registro.js
+++ routes/registro.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const { getFranela } = require('../controllers/registro');
 const asyncHandler = require('../middlewares/async-handler');
 const loginUser = require('../middlewares/loginUser');
 const router = express.Router();
 
-router.get('/franela', asyncHandler())
+router.get('/franela', asyncHandler(getFranela))
 
 module.exports = router;
