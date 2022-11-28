fa770ee35e43278bd5046cd57c4e54081649c079
Jfilippo06 <jfilippo04@gmail.com>
2022-11-28 20:27:43 +0100

    ejs

---
+++ controllers/venta.js
@@ -0,0 +1,7 @@
+const buscarClienteController = async (req, res) => {
+    res.render("pages/venta/buscar-cliente")
+};
+
+module.exports = {
+    buscarClienteController,
+}
--- routes/venta.js
+++ routes/venta.js
@@ -1,7 +1,10 @@
-const express = require('express');
-const asyncHandler = require('../middlewares/async-handler');
-const loginUser = require('../middlewares/loginUser');
+const express = require("express");
+const { buscarClienteController } = require("../controllers/venta");
+const asyncHandler = require("../middlewares/async-handler");
+const loginUser = require("../middlewares/loginUser");
 const verifyAdmin = require("../middlewares/verifyAdmin");
 const router = express.Router();
+
+router.get("/", loginUser, asyncHandler(buscarClienteController));
 
 module.exports = router;
