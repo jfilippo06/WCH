82892b4692a3f895a6e0a278e5c9a6727f95792f
Jfilippo06 <jfilippo04@gmail.com>
2022-12-01 19:44:00 +0100

    seeder cliente

--- seeders/20221128183027-Cliente.js
+++ seeders/20221128183027-Cliente.js
@@ -3,15 +3,24 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-     */
+    await queryInterface.bulkInsert(
+      "Clientes",
+      [
+        {
+          nombre: "Jose",
+          cedula: 27307757,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          nombre: "John Doe",
+          cedula: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
   async down(queryInterface, Sequelize) {
